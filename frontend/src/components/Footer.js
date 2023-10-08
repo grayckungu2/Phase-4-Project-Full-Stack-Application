0caafda0216333b3333886450365431a38afcada
duan spot <brandennjonge@gmail.com>
2023-10-08 11:35:12 +0300

    changed styling of homepage button

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
+
 const Footer = () => {
-    const [showEvents, setShowEvents] = useState(true);
+  const [showEvents, setShowEvents] = useState(true);
   const [events] = useState([
     { title: "Branden", id: 1 },
     { title: "Grace", id: 2 },
     { title: "Joshua", id: 3 },
     { title: "Gabriel", id: 4 },
   ]);
-    return ( 
-        <>
 
-{showEvents && (
-        <div className="center-container">
+  return (
+    <>
+      {showEvents && (
+        <div className="d-flex justify-content-center">
           <button type="button" className="btn btn-info" onClick={() => setShowEvents(false)}>Hide Creators</button>
         </div>
       )}
       {!showEvents && (
-        <div className="center-container">
+        <div className="d-flex justify-content-center">
           <button type="button" className="btn btn-primary" onClick={() => setShowEvents(true)}>Show Creators</button>
         </div>
       )}
@@ -31,13 +32,13 @@ const Footer = () => {
           ))}
         </div>
       )}
-     <footer className="bg-dark mt-2 text-white text-center">
-      <p>&copy;2023.All Rights Reserved</p>
-      <p>Email us at moviereviews@email.com</p>
-      <p>call us @0938-2399-2399</p>
-    </footer>
-        </>
-     );
+      <footer className="bg-dark mt-2 text-white text-center">
+        <p>&copy;2023. All Rights Reserved</p>
+        <p>Email us at moviereviews@email.com</p>
+        <p>Call us @0938-2399-2399</p>
+      </footer>
+    </>
+  );
 }
- 
+
 export default Footer;
